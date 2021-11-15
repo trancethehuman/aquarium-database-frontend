cc608e995098d4334fcdb3d13b686b3b45f12fc4
Hai Nghiem <hainghiem375@gmail.com>
2021-11-15 17:45:53 -0330

    router working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import {
+  BrowserRouter,
+  Link,
+  Route,
+  Routes
+} from "react-router-dom";
 
-function App() {
+import {Home} from './routes/Home';
+import {About} from './routes/About';
+import {Browse} from './routes/Browse';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App"/>
+      <Routes>
+        <Route exact path="/about" element={<About/>}></Route>
+        <Route exact path="/browse" element={<Browse/>}></Route>
+        <Route exact path="/" element={<Home/>}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/routes/Browse.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const Browse = () => {
+    // const auth = null //authorized route, no? Just putting this here in case we do auth
+    return(
+        <p>Browse Fish</p>
+    )
+}
