d0ba87a86ca853bec889f3fb627a5cfb40a2ca14
Hai Nghiem <hainghiem375@gmail.com>
2021-11-20 15:14:40 -0330

    search bar component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import '../css/SearchBar.css';
 
 //Search Bar component. Used on StandardNavBar and anywhere you want
 export const SearchBar = () => {
     return(
-
-    )
-}
+        <div className="searchContainer">
+            <form className="search">
+                <input className="searchBar" type="text" placeholder="What are you looking for? 🐠" />
+                <input className="searchSubmit" type="submit" value="🔍" />
+            </form>
+        </div>
+    );
+};
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import "../css/Home.css";
 import { HomeNavBar } from '../components/HomeNavBar';
+import { SearchBar } from '../components/SearchBar';
 
 export const Home = () => {
     return (
         <>
             <HomeNavBar />
-            <div className="searchContainer">
-                <form className="search">
-                    <input className="searchBar" type="text" placeholder="What are you looking for? 🐠" />
-                    <input className="searchSubmit" type="submit" value="🔍" />
-                </form>
-            </div>
+            <SearchBar />
         </>
     );
 }
