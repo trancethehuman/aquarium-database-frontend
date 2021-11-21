a0a6e7f2685e71db8f65ad19d2e39b2d5882ba1c
Hai Nghiem <hainghiem375@gmail.com>
2021-11-21 16:32:44 -0330

    fixed navbar spacing

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -6,12 +6,10 @@ import MagnifyingGlass from '../../assets/Search_duotone_icon.svg';
 export const SearchBar = () => {
     return(
         <div className="searchContainer">
-            <form className="search">
-                <input className="SearchText" type="text" placeholder="What 🐠 are you looking for?" />
-                <button className="SearchSubmitButton">
-                    <img className='SearchButtonImage' alt='Search Button' src={MagnifyingGlass}></img>
-                </button>
-            </form>
+            <input className="SearchText" type="text" placeholder="What 🐠 are you looking for?" />
+            <button className="SearchSubmitButton">
+                <img className='SearchButtonImage' alt='Search Button' src={MagnifyingGlass}></img>
+            </button>
         </div>
     );
 };
--- src/components/SearchBar/SearchBarCompact.js
+++ src/components/SearchBar/SearchBarCompact.js
@@ -5,13 +5,11 @@ import MagnifyingGlass from '../../assets/Search_duotone_icon.svg';
 //Search bar but more compact than standard search bar. To be used on nav bars and small places :D
 export const SearchBarCompact = () => {
     return(
-        <div className="searchContainer">
-            <form className="search">
-                <input className="SearchText" type="text" placeholder="What 🐠 are you looking for?" />
-                <button className="SearchSubmitButton">
-                    <img className='SearchButtonImage' alt='Search Button' src={MagnifyingGlass}></img>
-                </button>
-            </form>
+        <div className="CompactSearchContainer">
+            <input className="CompactSearchText" type="text" placeholder="What 🐠 are you looking for?" />
+            <button className="CompactSearchSubmitButton">
+                <img className='CompactSearchButtonImage' alt='Compact Search Button' src={MagnifyingGlass}></img>
+            </button>
         </div>
     )
 }
